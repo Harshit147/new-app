cd3a9dd0c1042e8fe5dc4d779b325e50acebc356
Harshit147 <harshitparashar345@gmail.com>
2020-10-06 23:42:08 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HARSHIT Parashar
         </p>
         <a
           className="App-link"
